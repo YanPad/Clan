42fd702f417457bf8f3a7b7a52b7d6788d866031
YanPad <silaelixira@gmail.com>
2020-07-09 11:46:57 +0300

    start

---
+++ lib/clan.ex
@@ -0,0 +1,18 @@
+defmodule Clan do
+  @moduledoc """
+  Documentation for Clan.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Clan.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
